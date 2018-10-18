682bed59540b2b433f8a2bed2dfdb2e73545fe87
borkode <31637006+borkode@users.noreply.github.com>
2018-10-18 13:48:29 -0400

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -1,1 +1,5 @@
 // pacman.js
+var ghosts_left  = ['ghost_1.png','ghost_3.png','ghost_5.png','ghost_7.png'];
+var ghosts_right = ['ghost_0.png','ghost_2.png','ghost_4.png','ghost_6.png'];
+var ghost_colors = ['red','orange','pink','blue'];
+var pacman = 'pacman-move.gif';
