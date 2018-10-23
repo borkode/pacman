ff2fb0ceacd5881af8f6da7ead440c2e02c84bb4
borkode <31637006+borkode@users.noreply.github.com>
2018-10-23 16:16:13 -0400

    Update red.js

--- ghostscripts/red.js
+++ ghostscripts/red.js
@@ -1,5 +1,5 @@
 // ghostscripts/red.js
-var ghost_left  = 'ghost_1.png'
-var ghost_right = 'ghost_0.png'
+var l = ghosts_left[0]
+var r = ghosts_right[0]
 var ghosts_el = document.getElementById('redghost')
-
+ghosts_el.setAttribute('src',l);
