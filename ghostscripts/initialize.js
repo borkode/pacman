5656a8042911934d384771c2cfa84a288c9fe2dc
borkode <31637006+borkode@users.noreply.github.com>
2018-10-23 16:20:13 -0400

    Create initialize.js

---
+++ ghostscripts/initialize.js
@@ -0,0 +1,19 @@
+// /ghostscripts/initialize.js
+var ghosts_el = [
+document.getElementById('redghost'),
+document.getElementById('orangeghost'),
+document.getElementById('pinkghost'),
+document.getElementById('blueghost')
+]
+
+ghosts_el[0].setAttribute('width', pixel_size);
+ghosts_el[0].setAttribute('height', pixel_size);
+ghosts_el[1].setAttribute('width', pixel_size);
+ghosts_el[1].setAttribute('height', pixel_size);
+ghosts_el[2].setAttribute('width', pixel_size);
+ghosts_el[2].setAttribute('height', pixel_size);
+ghosts_el[3].setAttribute('width', pixel_size);
+ghosts_el[3].setAttribute('height', pixel_size);
+
+var ghosts_left  = ['ghost_1.png','ghost_3.png','ghost_5.png','ghost_7.png'];
+var ghosts_right = ['ghost_0.png','ghost_2.png','ghost_4.png','ghost_6.png'];
