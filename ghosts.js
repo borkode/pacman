bd4afee44aecc937638679fbef424773850b3faf
borkode <31637006+borkode@users.noreply.github.com>
2018-10-23 16:14:02 -0400

    Update ghosts.js

--- ghosts.js
+++ ghosts.js
@@ -1,1 +1,16 @@
-alert(layout[0]);
+// ghosts.js
+var ghosts_el = [
+document.getElementById('redghost'),
+document.getElementById('orangeghost'),
+document.getElementById('pinkghost'),
+document.getElementById('blueghost')
+]
+
+ghosts_el[0].setAttribute('width', pixel_size);
+ghosts_el[0].setAttribute('height', pixel_size);
+ghosts_el[1].setAttribute('width', pixel_size);
+ghosts_el[1].setAttribute('height', pixel_size);
+ghosts_el[2].setAttribute('width', pixel_size);
+ghosts_el[2].setAttribute('height', pixel_size);
+ghosts_el[3].setAttribute('width', pixel_size);
+ghosts_el[3].setAttribute('height', pixel_size);
