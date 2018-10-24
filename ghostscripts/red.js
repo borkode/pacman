23cf2b23b7e8bf3d9104f2e62b650169200c43c7
borkode <31637006+borkode@users.noreply.github.com>
2018-10-24 09:19:33 -0400

    Update red.js

--- ghostscripts/red.js
+++ ghostscripts/red.js
@@ -1,4 +1,5 @@
 // ghostscripts/red.js
+// Blinky is the only ghost to target the player directly
 var l = '/pacman/ghost_1.png'
 var r = '/pacman/ghost_0.png'
 var ghosts_el = document.getElementById('redghost')
