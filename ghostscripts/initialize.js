05e1ea604591675d80b5a37755512639c5e14282
borkode <31637006+borkode@users.noreply.github.com>
2018-10-29 10:47:07 -0400

    Update initialize.js

--- ghostscripts/initialize.js
+++ ghostscripts/initialize.js
@@ -17,3 +17,7 @@ ghosts_el[3].setAttribute('height', pixel_size);
 
 var ghosts_left  = ['ghost_1.png','ghost_3.png','ghost_5.png','ghost_7.png'];
 var ghosts_right = ['ghost_0.png','ghost_2.png','ghost_4.png','ghost_6.png'];
+
+ghosts_el[0].style.position='absolute';
+ghosts_el[0].style.left='33px';
+ghosts_el[0].style.top='33px';
