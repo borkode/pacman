df58ee0dd842b22fa3d4f8ea49989f8fa663b05b
borkode <31637006+borkode@users.noreply.github.com>
2018-10-29 10:16:32 -0400

    Update red.js

--- ghostscripts/red.js
+++ ghostscripts/red.js
@@ -4,3 +4,23 @@ var l = '/pacman/ghost_1.png'
 var r = '/pacman/ghost_0.png'
 var ghosts_el = document.getElementById('redghost')
 ghosts_el.setAttribute('src',l);
+
+setInterval(function(){
+
+},250);
+setInterval(function(){
+  easystar.findPath(posx, posy, tox, toy, function(path) {
+	alert(path);
+	if (path != null) {
+var x=0;
+		var pathc = [];
+			pathc.push(path[1].x);
+			pathc.push(path[1].y);
+    return pathc
+	}else{
+  return [posx,posy]
+  }
+	
+});
+	easystar.calculate();	
+},1);
