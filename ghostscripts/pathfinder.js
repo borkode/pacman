bc91e348a602fa72ad871ff12aecd6f26d5464d6
borkode <31637006+borkode@users.noreply.github.com>
2018-10-29 09:35:40 -0400

    Update pathfinder.js

--- ghostscripts/pathfinder.js
+++ ghostscripts/pathfinder.js
@@ -30,6 +30,6 @@ easystar.findPath(1, 1, 4, 1, function(path) {
 		alert("Path was not found.");
 	} else {
 		alert("Path was found. The first Point is " + path[0].x + " " + path[0].y);
-	}
 alert(path);
+	}
 });
