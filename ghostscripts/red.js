a9317ea1c123177bca5f78354f3b270dabcb8029
borkode <31637006+borkode@users.noreply.github.com>
2018-10-29 10:56:46 -0400

    Update red.js

--- ghostscripts/red.js
+++ ghostscripts/red.js
@@ -6,28 +6,12 @@ var ghosts_el = document.getElementById('redghost')
 ghosts_el.setAttribute('src',l);
 
 
-setInterval(function(){
-	var posx = (document.getElementById('redghost').getBoundingClientRect().left-8)/pixel_size
-	var posy = (document.getElementById('redghost').getBoundingClientRect().left-8)/pixel_size
-	var tox=pacpos[0]
-	var toy=pacpos[1]
-  easystar.findPath(posx, posy, tox, toy, function(path) {
-	if (path != null) {
-var x=0;
-		var pathc = [];
-			pathc.push(path[1].x);
-			pathc.push(path[1].y);
-    return pathc
-	}else{
+
   
-  }
-	
-});
-	easystar.calculate();	
-},1);
 
 setInterval(function(){
 document.getElementById('redghost').style.postition = 'absolute';
+	var pathc=setInterval(function(){var posx = (document.getElementById('redghost').getBoundingClientRect().left-8)/pixel_size;var posy = (document.getElementById('redghost').getBoundingClientRect().left-8)/pixel_size;var tox=pacpos[0];var toy=pacpos[1];easystar.findPath(posx, posy, tox, toy, function(path) {if (path != null) {var x=0;var pathc = [];pathc.push(path[1].x);pathc.push(path[1].y);return pathc}else{return [posx,posy];}});easystar.calculate();	},1);
 	document.getElementById('redghost').style.left=pathc[0]*pixel_size+8
 	document.getElementById('redghost').style.top=pathc[1]*pixel_size+8
 },250);
