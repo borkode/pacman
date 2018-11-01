05eafa72fa7ce367728ca0e396017b58680056f8
borkode <31637006+borkode@users.noreply.github.com>
2018-11-01 10:12:31 -0400

    Update red.js

--- ghostscripts/red.js
+++ ghostscripts/red.js
@@ -8,7 +8,7 @@ var pathe = [];
 setInterval(function(){
 document.getElementById('redghost').style.postition = 'absolute';
 var posx = (document.getElementById('redghost').getBoundingClientRect().left-8)/pixel_size;var posy = (document.getElementById('redghost').getBoundingClientRect().left-8)/pixel_size;var tox=pacpos[0];var toy=pacpos[1];
-pathe=easystar.findPath(posx, posy, tox, toy, function(path) {if (path != null) {console.log(path);var z=0;var pathc = [path[1].x,path[1].y];}else{pathc= [posx,posy];};return pathc;});
+pathe=easystar.findPath(posx, posy, tox, toy, function(path) {if (path != null) {var pathc = [path[1].x,path[1].y];}else{pathc= [posx,posy];};return pathc;});
 	easystar.calculate();
 	document.getElementById('redghost').style.left=pathe[0]*pixel_size+8;document.getElementById('redghost').style.top=pathe[1]*pixel_size+8;
 },250);
